99bf5fd256fe0d4934e98d64a84160f4b41eb4a2
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-08-08 23:38:26 -0500

    New UI project coyp from TFS and comment some items to fix later

--- Ginger-PACT-Plugin/PACTService.cs
+++ Ginger-PACT-Plugin/PACTService.cs
@@ -26,7 +26,133 @@ namespace Ginger_PACT_Plugin
             SV = new ServiceVirtualization(port);
 
             //ExInfo
-            GA.ExInfo = "PACT Mock Server Started on port: " + port + " " + SV.MockProviderServiceBaseUri; ;
+            GA.ExInfo = "PACT Mock Server Started on port: " + port + " " + SV.MockProviderServiceBaseUri;
         }
+
+
+        [GingerAction("StartPACTServer", "Start PACT Server")]
+        public void StopPACTServer(ref GingerAction GA)
+        {
+
+            // Act
+            SV.MockProviderService.Stop();
+            SV = null;
+            //ExInfo
+            GA.ExInfo = "PACT Mock Server Stopped";
+        }
+
+        [GingerAction("LoadInteractionsFile", "Load Interactions File")]
+        public void LoadInteractionsFile(ref GingerAction GA, string fileName)
+        {
+            if (SV == null)
+            {
+                GA.AddError("LoadInteractionsFile","Error: Service Virtualization not started yet");
+                return;
+            }
+            
+            //if (s.StartsWith("~"))
+            //    s = Path.GetFullPath(s.Replace("~", act.SolutionFolder));
+            //if (File.Exists(s))
+            //{
+            //    int count = SV.LoadInteractions(s);
+            //    act.ExInfo += "Interaction file loaded: '" + s + "', " + count + " Interactions loaded";
+            //    act.AddOutput("Interaction Loaded", count + "");
+            //}
+            //else
+            //{
+            //    act.Error += "Interaction file not found - " + s;
+            //}
+
+            // Act
+
+            //ExInfo
+            GA.ExInfo = "not implemented yet";
+        }
+
+
+        [GingerAction("ClearInteractions", "Clear Interactions")]
+        public void ClearInteractions(ref GingerAction GA)
+        {
+            if (SV == null)
+            {
+                GA.AddError("LoadInteractionsFile", "Error: Service Virtualization not started yet");
+                return;
+            }
+
+            //if (s.StartsWith("~"))
+            //    s = Path.GetFullPath(s.Replace("~", act.SolutionFolder));
+            //if (File.Exists(s))
+            //{
+            //    int count = SV.LoadInteractions(s);
+            //    act.ExInfo += "Interaction file loaded: '" + s + "', " + count + " Interactions loaded";
+            //    act.AddOutput("Interaction Loaded", count + "");
+            //}
+            //else
+            //{
+            //    act.Error += "Interaction file not found - " + s;
+            //}
+
+            // Act
+            SV.ClearInteractions();
+            
+            //ExInfo
+            GA.ExInfo = "Interactions cleared";
+        }
+
+        // TODO: LoadInteractionsFolder
+
+        //case cAddInteraction:
+        //    if (SV == null)
+        //    {
+        //        act.Error += "Error: Service Virtualization not started yet";
+        //        return;
+        //    }
+
+        //    //TODO: get it from the edit page
+        //    ProviderServiceInteraction PSI = new ProviderServiceInteraction();
+        //    PSI.ProviderState = act.GetOrCreateParam("ProviderState").GetValueAsString();
+        //    PSI.Description = act.GetOrCreateParam("Description").GetValueAsString();
+        //    PSI.Request = new ProviderServiceRequest();
+
+
+        //    string HTTPType = act.GetOrCreateParam("RequestType").GetValueAsString();
+        //    switch (HTTPType)
+        //    {
+        //        case "Get":
+        //            PSI.Request.Method = HttpVerb.Get;
+        //            break;
+        //        case "Post":
+        //            PSI.Request.Method = HttpVerb.Post;
+        //            break;
+        //                case "PUT":
+        //            PSI.Request.Method = HttpVerb.Put;
+        //            break;
+
+        //            //TODO: add all the rest and include default for err
+        //    }
+
+        //    PSI.Request.Path = act.GetOrCreateParam("Path").GetValueAsString();
+        //    Dictionary<string, string> d = new Dictionary<string, string>();
+        //    d.Add("Accept", "application/json");  //TODO: fixme
+        //    PSI.Request.Headers = d;
+        //    PSI.Response = new ProviderServiceResponse();
+        //    PSI.Response.Status = 200;  //TODO: fixme
+        //    Dictionary<string, string> r = new Dictionary<string, string>();
+        //    r.Add("Content-Type", "application/json; charset=utf-8");   //TODO: fixme
+        //    PSI.Response.Headers = r;
+        //    PSI.Response.Body = act.GetOrCreateParam("ResposneBody").GetValueAsString();
+
+        //    SV.AddInteraction(PSI);
+
+        //    act.ExInfo += "Interaction added";
+
+        //    break;
+        //case cSaveInteractions:
+        //    SV.SaveInteractions();
+        //    act.ExInfo += "Interactions saved to file";
+        //    // TODO: add file name to ex info
+        //    break;
+
+
     }
 }
