0f4a4a00988ae13e0244001cce9e24fb3914acc5
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-08-13 23:11:28 -0500

    fix LoadInteractionsFile test

--- GingerPACTPluginTest/UnitTest1.cs
+++ GingerPACTPluginTest/UnitTest1.cs
@@ -38,7 +38,7 @@ namespace GingerPACTPluginTest
             service.LoadInteractionsFile(ref GA2, TestResources.GetTestResourcesFile("Sample.PACT.json"));
 
             //Assert
-            Assert.AreEqual("PACT Mock Server Started on port: 55555 http://localhost:5555", GA.ExInfo, "Exinfo message");
+            Assert.AreEqual("PACT Mock Server Started on port: 5555 http://localhost:5555", GA.ExInfo, "Exinfo message");
             Assert.AreEqual(null, GA.Errors, "No Errors");
 
         }
